b0f77af3f50d3fa27de39c5bf028e1a7330bc68c
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-19 21:08:05 +0700

    #11 - Add data.sql file in customer-service module

---
+++ customer-service/src/main/resources/data.sql
@@ -0,0 +1,100 @@
+INSERT INTO customer
+VALUES (1, 'Adriana C. Ocampo Uria', 15078);
+INSERT INTO customer
+VALUES (2, 'Albert Einstein', 16228);
+INSERT INTO customer
+VALUES (3, 'Anna K. Behrensmeyer', 14307);
+INSERT INTO customer
+VALUES (4, 'Blaise Pascal', 17597);
+INSERT INTO customer
+VALUES (5, 'Caroline Herschel', 27888);
+INSERT INTO customer
+VALUES (6, 'Cecilia Payne-Gaposchkin', 19215);
+INSERT INTO customer
+VALUES (7, 'Chien-Shiung Wu', 19004);
+INSERT INTO customer
+VALUES (8, 'Dorothy Hodgkin', 14736);
+INSERT INTO customer
+VALUES (9, 'Edmond Halley', 28810);
+INSERT INTO customer
+VALUES (10, 'Edwin Powell Hubble', 22208);
+INSERT INTO customer
+VALUES (11, 'Elizabeth Blackburn', 17373);
+INSERT INTO customer
+VALUES (12, 'Enrico Fermi', 20069);
+INSERT INTO customer
+VALUES (13, 'Erwin Schroedinger', 12671);
+INSERT INTO customer
+VALUES (14, 'Flossie Wong-Staal', 21632);
+INSERT INTO customer
+VALUES (15, 'Frieda Robscheit-Robbins', 12593);
+INSERT INTO customer
+VALUES (16, 'Geraldine Seydoux', 17934);
+INSERT INTO customer
+VALUES (17, 'Gertrude B. Elion', 14989);
+INSERT INTO customer
+VALUES (18, 'Ingrid Daubechies', 21515);
+INSERT INTO customer
+VALUES (19, 'Jacqueline K. Barton', 19879);
+INSERT INTO customer
+VALUES (20, 'Jane Goodall', 24097);
+INSERT INTO customer
+VALUES (21, 'Jocelyn Bell Burnell', 14082);
+INSERT INTO customer
+VALUES (22, 'Johannes Kepler', 12047);
+INSERT INTO customer
+VALUES (23, 'Lene Vestergaard Hau', 18894);
+INSERT INTO customer
+VALUES (24, 'Lise Meitner', 20469);
+INSERT INTO customer
+VALUES (25, 'Lord Kelvin', 21535);
+INSERT INTO customer
+VALUES (26, 'Maria Mitchell', 25147);
+INSERT INTO customer
+VALUES (27, 'Marie Curie', 22298);
+INSERT INTO customer
+VALUES (28, 'Max Born', 17008);
+INSERT INTO customer
+VALUES (29, 'Max Planck', 23144);
+INSERT INTO customer
+VALUES (30, 'Melissa Franklin', 20864);
+INSERT INTO customer
+VALUES (31, 'Michael Faraday', 23597);
+INSERT INTO customer
+VALUES (32, 'Mildred S. Dresselhaus', 22847);
+INSERT INTO customer
+VALUES (33, 'Nicolaus Copernicus', 27106);
+INSERT INTO customer
+VALUES (34, 'Niels Bohr', 28019);
+INSERT INTO customer
+VALUES (35, 'Patricia S. Goldman-Rakic', 28926);
+INSERT INTO customer
+VALUES (36, 'Patty Jo Watson', 17278);
+INSERT INTO customer
+VALUES (37, 'Polly Matzinger', 26515);
+INSERT INTO customer
+VALUES (38, 'Richard Phillips Feynman', 24328);
+INSERT INTO customer
+VALUES (39, 'Rita Levi-Montalcini', 15093);
+INSERT INTO customer
+VALUES (40, 'Rosalind Franklin', 13003);
+INSERT INTO customer
+VALUES (41, 'Ruzena Bajcsy', 28853);
+INSERT INTO customer
+VALUES (42, 'Sarah Boysen', 27400);
+INSERT INTO customer
+VALUES (43, 'Shannon W. Lucid', 19740);
+INSERT INTO customer
+VALUES (44, 'Shirley Ann Jackson', 15031);
+INSERT INTO customer
+VALUES (45, 'Sir Ernest Rutherford', 28403);
+INSERT INTO customer
+VALUES (46, 'Sir Isaac Newton', 24683);
+INSERT INTO customer
+VALUES (47, 'Stephen Hawking', 29087);
+INSERT INTO customer
+VALUES (48, 'Werner Karl Heisenberg', 29076);
+INSERT INTO customer
+VALUES (49, 'Wilhelm Conrad Roentgen', 25786);
+INSERT INTO customer
+VALUES (50, 'Wolfgang Ernst Pauli', 17054);
