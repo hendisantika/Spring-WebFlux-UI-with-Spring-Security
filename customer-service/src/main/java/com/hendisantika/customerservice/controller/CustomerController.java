af6d3f0e48b3f80450d7ac4d192d0474099d4870
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-19 21:14:19 +0700

    #16 - Add getData route in CustomerController in customer-service module

--- customer-service/src/main/java/com/hendisantika/customerservice/controller/CustomerController.java
+++ customer-service/src/main/java/com/hendisantika/customerservice/controller/CustomerController.java
@@ -2,7 +2,11 @@ package com.hendisantika.customerservice.controller;
 
 import com.hendisantika.customerservice.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +21,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     @Autowired
     private CustomerRepository customerRepository;
+
+    @GetMapping(value = "/data")
+    public Mono<String> getData(ServerHttpRequest request, ServerHttpResponse response) {
+
+        String path = request.getURI().getPath();
+        Mono<String> data = Mono.just("Invoking " + path + " !!");
+        return data;
+    }
 }
