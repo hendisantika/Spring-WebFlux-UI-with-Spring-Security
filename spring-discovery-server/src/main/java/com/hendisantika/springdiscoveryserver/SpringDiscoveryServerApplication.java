5571e200e6bf8256f6e85b8f484959f17a546fb7
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-19 20:35:57 +0700

    #2 - Add spring-discovery-server module

---
+++ spring-discovery-server/src/main/java/com/hendisantika/springdiscoveryserver/SpringDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springdiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ spring-discovery-server/src/test/java/com/hendisantika/springdiscoveryserver/SpringDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
