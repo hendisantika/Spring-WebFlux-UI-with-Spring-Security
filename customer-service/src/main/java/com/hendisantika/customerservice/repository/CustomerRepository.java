7a8c43778080614f43603b00c8ed4b6a6d446589
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-19 21:12:27 +0700

    #14 - Add Customer Repository in customer-service module

---
+++ customer-service/src/main/java/com/hendisantika/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.hendisantika.customerservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-WebFlux-UI-with-Spring-Security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/09/20
+ * Time: 21.11
+ */
+public interface CustomerRepository extends JpaRepository<src.main.java.com.hendisantika.customerservice.entity.Customer, Long> {
+}
