e98af05f584f5e1fd3a179e52e43fd2d84d2328a
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-20 16:37:29 +0700

    #21 - Add spring-gateway module

---
+++ spring-gateway/src/main/java/com/hendisantika/springgateway/SpringGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGatewayApplication.class, args);
+    }
+
+}
