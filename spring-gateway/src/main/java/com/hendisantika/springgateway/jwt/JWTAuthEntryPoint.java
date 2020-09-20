0912b440bfc2205a93314e1ee44f52858cdad748
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-20 17:02:36 +0700

    #40 - Add JWTAuthEntryPoint in spring-gateway module

---
+++ spring-gateway/src/main/java/com/hendisantika/springgateway/jwt/JWTAuthEntryPoint.java
@@ -0,0 +1,34 @@
+package com.hendisantika.springgateway.jwt;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-WebFlux-UI-with-Spring-Security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/09/20
+ * Time: 17.02
+ */
+@Component
+public class JWTAuthEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(JWTAuthEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        logger.error("Unauthorized error: {}", authException.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+    }
+}
