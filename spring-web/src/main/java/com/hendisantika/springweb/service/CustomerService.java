54d7c266c4f27f39853eaea2e101838002b44e0c
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-21 05:14:22 +0700

    #71 - Add findAll method in spring-web module

--- spring-web/src/main/java/com/hendisantika/springweb/service/CustomerService.java
+++ spring-web/src/main/java/com/hendisantika/springweb/service/CustomerService.java
@@ -1,9 +1,20 @@
 package com.hendisantika.springweb.service;
 
+import com.hendisantika.springweb.dto.CustomerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +33,23 @@ public class CustomerService {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    public List<CustomerDTO> findAll(String token) {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+            HttpEntity<String> entity = new HttpEntity<>("body", headers);
+            ResponseEntity<List<CustomerDTO>> record = restTemplate.exchange(this.gatewayUrl + "/findAll",
+                    HttpMethod.GET,
+                    entity, new ParameterizedTypeReference<List<CustomerDTO>>() {
+                    });
+            if (record != null && record.hasBody()) {
+                return record.getBody();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<CustomerDTO>();
+    }
 }
