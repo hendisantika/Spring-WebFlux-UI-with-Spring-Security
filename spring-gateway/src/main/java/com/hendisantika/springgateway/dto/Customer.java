3fcf04aebc4202a3cd0bec380c1ea97507ef0b2b
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-20 16:47:22 +0700

    #29 - Add Customer DTO in spring-gateway module

---
+++ spring-gateway/src/main/java/com/hendisantika/springgateway/dto/Customer.java
@@ -0,0 +1,64 @@
+package com.hendisantika.springgateway.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-WebFlux-UI-with-Spring-Security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/09/20
+ * Time: 16.46
+ */
+public class Customer {
+    private Long id;
+
+    private String name;
+
+    private Long membershipId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getMembershipId() {
+        return membershipId;
+    }
+
+    public void setMembershipId(Long membershipId) {
+        this.membershipId = membershipId;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Customer other = (Customer) obj;
+        if (id == null) {
+            return other.id == null;
+        } else return id.equals(other.id);
+    }
+}
