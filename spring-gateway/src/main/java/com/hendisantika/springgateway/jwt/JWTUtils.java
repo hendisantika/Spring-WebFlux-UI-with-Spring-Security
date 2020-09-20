2460b855dc4adb805afe9e247e1096ed481ab98f
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-20 17:05:19 +0700

    #43 - Add validateJwtToken in JWTUtils in spring-gateway module

--- spring-gateway/src/main/java/com/hendisantika/springgateway/jwt/JWTUtils.java
+++ spring-gateway/src/main/java/com/hendisantika/springgateway/jwt/JWTUtils.java
@@ -1,8 +1,12 @@
 package com.hendisantika.springgateway.jwt;
 
 import com.hendisantika.springgateway.service.UserDetailsImpl;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -41,5 +45,28 @@ public class JWTUtils {
                 .setExpiration(new Date((new Date()).getTime() + jwtTokenValidity))
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String jwt) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwt);
+            return true;
+        } catch (SignatureException e) {
+            logger.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            logger.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
     }
 }
