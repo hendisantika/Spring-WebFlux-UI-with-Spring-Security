119f5289b146159cce0b90868c736188df70a26b
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-20 17:08:13 +0700

    #49 - Add LoginRequest in spring-gateway module

---
+++ spring-gateway/src/main/java/com/hendisantika/springgateway/reqres/LoginRequest.java
@@ -0,0 +1,40 @@
+package com.hendisantika.springgateway.reqres;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-WebFlux-UI-with-Spring-Security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/09/20
+ * Time: 17.07
+ */
+public class LoginRequest {
+    private String username;
+
+    private String password;
+
+    public LoginRequest() {
+    }
+
+    public LoginRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
