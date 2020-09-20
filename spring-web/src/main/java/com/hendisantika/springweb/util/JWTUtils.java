26fd79eabc47bfcdafbbcd8ac83e18248ff65b9c
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-21 05:10:05 +0700

    #66 - Add JWTUtils method in spring-web module

---
+++ spring-web/src/main/java/com/hendisantika/springweb/util/JWTUtils.java
@@ -0,0 +1,27 @@
+package com.hendisantika.springweb.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-WebFlux-UI-with-Spring-Security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/09/20
+ * Time: 05.09
+ */
+@Component
+public class JWTUtils {
+    private static final Logger logger = LoggerFactory.getLogger(JWTUtils.class);
+
+    @Value("${jwt.security.key}")
+    private String jwtSecret;
+
+    // Token validity in milliseconds
+    @Value("${jwt.token.validity}")
+    private int jwtTokenValidity;
+}
