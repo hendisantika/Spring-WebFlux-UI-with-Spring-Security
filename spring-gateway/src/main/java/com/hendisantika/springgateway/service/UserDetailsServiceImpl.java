e1994ee947c9cb065a482292d36c57cc3fec275a
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-20 16:57:52 +0700

    #39 - Add UserDetailsServiceImpl in spring-gateway module

---
+++ spring-gateway/src/main/java/com/hendisantika/springgateway/service/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.hendisantika.springgateway.service;
+
+import com.hendisantika.springgateway.entity.User;
+import com.hendisantika.springgateway.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-WebFlux-UI-with-Spring-Security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/09/20
+ * Time: 16.57
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
