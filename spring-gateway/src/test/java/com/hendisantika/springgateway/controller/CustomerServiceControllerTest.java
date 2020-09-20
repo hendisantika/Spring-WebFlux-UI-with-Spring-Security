be931edf05708d17a6c69cf992e877a06b9a8c1a
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-21 04:57:11 +0700

    #56 - Add setup in spring-gateway module

--- spring-gateway/src/test/java/com/hendisantika/springgateway/controller/CustomerServiceControllerTest.java
+++ spring-gateway/src/test/java/com/hendisantika/springgateway/controller/CustomerServiceControllerTest.java
@@ -1,8 +1,12 @@
 package com.hendisantika.springgateway.controller;
 
+import com.hendisantika.springgateway.reqres.LoginRequest;
+import com.hendisantika.springgateway.reqres.LoginResponse;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
@@ -21,4 +25,17 @@ public class CustomerServiceControllerTest {
     private final TestRestTemplate restTemplate = new TestRestTemplate();
     private String token;
 
+    @Before
+    public void setup() {
+
+        LoginRequest loginRequest = new LoginRequest("user1", "user1");
+        try {
+            ResponseEntity<LoginResponse> response = restTemplate.postForEntity("http://localhost:8080/authenticate",
+                    loginRequest, LoginResponse.class);
+            LoginResponse loginResponse = response.getBody();
+            token = loginResponse.getJwt();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
