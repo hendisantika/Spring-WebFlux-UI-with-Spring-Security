b47f9c6c37d926f464076799c6297b7e6132a0c5
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-20 16:51:39 +0700

    #34 - Add setup in spring-gateway module

--- spring-gateway/src/main/java/com/hendisantika/springgateway/service/CustomerServiceClient.java
+++ spring-gateway/src/main/java/com/hendisantika/springgateway/service/CustomerServiceClient.java
@@ -1,9 +1,13 @@
 package com.hendisantika.springgateway.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +28,13 @@ public class CustomerServiceClient {
     private RestTemplate restTemplate;
 
     private String customerServiceUrl;
+
+    @PostConstruct
+    public void setup() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("customer-service");
+        if (instances != null && instances.size() > 0) {
+            ServiceInstance serviceInstance = instances.get(0);
+            this.customerServiceUrl = serviceInstance.getUri().toString();
+        }
+    }
 }
