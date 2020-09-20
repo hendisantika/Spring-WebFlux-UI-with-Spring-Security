ffde02cda193d736894035690127e48bf968497a
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-20 17:06:57 +0700

    #46 - Add authenticationManagerBean & passwordEncoder bean in GatewaySecurityConfig in spring-gateway module

--- spring-gateway/src/main/java/com/hendisantika/springgateway/config/GatewaySecurityConfig.java
+++ spring-gateway/src/main/java/com/hendisantika/springgateway/config/GatewaySecurityConfig.java
@@ -5,10 +5,13 @@ import com.hendisantika.springgateway.jwt.JWTTokenFilter;
 import com.hendisantika.springgateway.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -43,5 +46,17 @@ public class GatewaySecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
