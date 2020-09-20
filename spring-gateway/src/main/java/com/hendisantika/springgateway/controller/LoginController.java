651c9f3e899bb88ab6d232c093e60c8502a4517e
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-20 17:09:41 +0700

    #51 - Add LoginController in spring-gateway module

---
+++ spring-gateway/src/main/java/com/hendisantika/springgateway/controller/LoginController.java
@@ -0,0 +1,34 @@
+package com.hendisantika.springgateway.controller;
+
+import com.hendisantika.springgateway.jwt.JWTUtils;
+import com.hendisantika.springgateway.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Spring-WebFlux-UI-with-Spring-Security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 20/09/20
+ * Time: 17.09
+ */
+@RestController
+public class LoginController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+}
