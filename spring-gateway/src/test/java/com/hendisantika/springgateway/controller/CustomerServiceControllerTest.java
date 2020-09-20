afb53d98b13131b7c120983b58212c178afef419
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-21 04:58:15 +0700

    #57 - Add findAllCustomersTest in spring-gateway module

--- spring-gateway/src/test/java/com/hendisantika/springgateway/controller/CustomerServiceControllerTest.java
+++ spring-gateway/src/test/java/com/hendisantika/springgateway/controller/CustomerServiceControllerTest.java
@@ -3,11 +3,18 @@ package com.hendisantika.springgateway.controller;
 import com.hendisantika.springgateway.reqres.LoginRequest;
 import com.hendisantika.springgateway.reqres.LoginResponse;
 import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Created by IntelliJ IDEA.
@@ -37,5 +44,16 @@ public class CustomerServiceControllerTest {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Test
+    public void findAllCustomersTest() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:8080/findAll", HttpMethod.GET,
+                new HttpEntity<>(headers), String.class);
+
+        System.out.println(response.getBody());
+        assertThat(response.getStatusCode().equals(HttpStatus.OK));
     }
 }
