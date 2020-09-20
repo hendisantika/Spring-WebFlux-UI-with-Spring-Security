b09a978f08d3c73a71ecfa099ec17fe119ed1a75
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-20 17:09:59 +0700

    #52 - Add authenticateUser in LoginController in spring-gateway module

--- spring-gateway/src/main/java/com/hendisantika/springgateway/controller/LoginController.java
+++ spring-gateway/src/main/java/com/hendisantika/springgateway/controller/LoginController.java
@@ -2,10 +2,23 @@ package com.hendisantika.springgateway.controller;
 
 import com.hendisantika.springgateway.jwt.JWTUtils;
 import com.hendisantika.springgateway.repository.UserRepository;
+import com.hendisantika.springgateway.reqres.LoginRequest;
+import com.hendisantika.springgateway.reqres.LoginResponse;
+import com.hendisantika.springgateway.service.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,4 +44,19 @@ public class LoginController {
     @Autowired
     private JWTUtils jwtUtils;
 
+    @PostMapping("/authenticate")
+    public ResponseEntity<LoginResponse> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new LoginResponse(jwt, userDetails.getUsername(), roles));
+    }
 }
