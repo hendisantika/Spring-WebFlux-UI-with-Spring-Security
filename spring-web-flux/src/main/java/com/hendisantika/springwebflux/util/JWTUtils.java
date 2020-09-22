73c8d5ed3283a3fbe4436f2b9686ab65ee0de6dc
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-23 06:48:27 +0700

    #93 - Add generateJwtToken & getUserNameFromJwtToken method in JWTUtils

--- spring-web-flux/src/main/java/com/hendisantika/springwebflux/util/JWTUtils.java
+++ spring-web-flux/src/main/java/com/hendisantika/springwebflux/util/JWTUtils.java
@@ -1,9 +1,14 @@
 package com.hendisantika.springwebflux.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +29,17 @@ public class JWTUtils {
     // Token validity in milliseconds
     @Value("${jwt.token.validity}")
     private int jwtTokenValidity;
+
+    public String generateJwtToken(Authentication authentication) {
+
+        String name =  authentication.getName();
+
+        return Jwts.builder().setSubject(name).setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtTokenValidity))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret).compact();
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+    }
 }
