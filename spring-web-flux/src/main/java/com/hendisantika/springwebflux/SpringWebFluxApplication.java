78f27f3ad2fa07533fe341e5c0d0b4d68d6f4910
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-22 22:09:33 +0700

    #80 - Add Spring Web Flux App module

---
+++ spring-web-flux/src/main/java/com/hendisantika/springwebflux/SpringWebFluxApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxApplication.class, args);
+	}
+
+}
---
+++ spring-web-flux/src/test/java/com/hendisantika/springwebflux/SpringWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
