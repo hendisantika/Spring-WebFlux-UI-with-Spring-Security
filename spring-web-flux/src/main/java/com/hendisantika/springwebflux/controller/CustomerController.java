75158dffa6aee22202103ce736f1dd193805d672
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-23 06:54:25 +0700

    #100 - Add findAll in CustomerController

--- spring-web-flux/src/main/java/com/hendisantika/springwebflux/controller/CustomerController.java
+++ spring-web-flux/src/main/java/com/hendisantika/springwebflux/controller/CustomerController.java
@@ -1,9 +1,15 @@
 package com.hendisantika.springwebflux.controller;
 
+import com.hendisantika.springwebflux.dto.Customer;
 import com.hendisantika.springwebflux.service.CustomerService;
 import com.hendisantika.springwebflux.util.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +28,12 @@ public class CustomerController {
 
     @Autowired
     private JWTUtils jwtUtils;
+
+    @GetMapping("/findAllCustomers")
+    public String findAll(Model model, Authentication authentication) {
+        String token = jwtUtils.generateJwtToken(authentication);
+        List<Customer> customers = this.customerServcie.findAllCustomers(token);
+        model.addAttribute("customers", customers);
+        return "customers";
+    }
 }
